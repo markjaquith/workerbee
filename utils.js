3d2278ea12ed3c16e6264bfc351e456da29f32b1
Mark Jaquith <mark@coveredwebservices.com>
2020-11-29 16:57:51 -0500

    Add getCookie and isRedirect back to utils

--- utils.js
+++ utils.js
@@ -7,3 +7,48 @@ export function redirect(url, status = 302) {
 
 	return new Response(response.body, response);
 }
+
+/**
+ * Whether the given response is a redirect.
+ *
+ * @param Response A response object.
+ * @return boolean
+ */
+export function isRedirect({ status, headers }) {
+	return status >= 300 && status < 400 && headers.get('location');
+}
+
+export function setRequestUrl(url, request, options = {}) {
+	return new Request(url, {
+		body: request.body,
+		headers: request.headers,
+		method: request.method,
+		redirect: request.redirect,
+		...options,
+	});
+}
+
+/**
+ * Gets the cookie with the name from the request headers
+ *
+ * @param {Request} request incoming Request
+ * @param {string} name of the cookie to get
+ */
+export function getCookie(request, name) {
+	let result = "";
+	const cookieString = request.headers.get("Cookie");
+
+	if (cookieString) {
+	 const cookies = cookieString.split(";");
+	 cookies.forEach(cookie => {
+	  const cookiePair = cookie.split("=", 2);
+	  const cookieName = cookiePair[0].trim();
+	  if (cookieName === name) {
+	   const cookieVal = cookiePair[1];
+	   result = cookieVal;
+	  }
+	 });
+	}
+
+	return result;
+}

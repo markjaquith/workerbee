4bec86b57a4eff4fd0b7eca1df6329f2150dcee3
Mark Jaquith <mark@jaquith.me>
2021-02-22 13:07:28 -0500

    Add optional param tests

--- src/Router.test.js
+++ src/Router.test.js
@@ -7,48 +7,43 @@ const ROOT_HANDLER = makeHandler();
 const POSTS_HANDLER = makeHandler();
 const POST_HANDLER = makeHandler();
 const POST_UPDATE_HANDLER = makeHandler();
+const OPTIONAL_PARAM_HANDLER = makeHandler();
 
 const DOMAIN = 'https://example.com';
+const GET = 'GET';
+const POST = 'POST';
+const ID_PARAM = { id: '123' };
 const router = new Router();
 router.get('/', ROOT_HANDLER);
 router.get('/posts', POSTS_HANDLER);
 router.get('/posts/:id', POST_HANDLER);
 router.post('/posts/:id', POST_UPDATE_HANDLER);
+router.get('/optional/:id?', OPTIONAL_PARAM_HANDLER);
 
 function makeGet(path) {
 	return httpMocks.createRequest({
-		method: 'GET', 
-		url: 'https://example.com' + path,
+		method: GET, 
+		url: DOMAIN + path,
 	});
 }
 
 function makePost(path) {
 	return httpMocks.createRequest({
 		method: 'POST',
-		url: 'https://example.com' + path,
+		url: DOMAIN + path,
 	});
+}
+
+function makeHandlerMatcher(method, handlers, params = {}) {
+	return { method, handlers, params };
 }
 
 test('Router', () => {
 	expect(makeGet('/').path).toBe('/');
-	expect(router.getRoute(makeGet('/'))).toMatchObject({
-		method: 'GET',
-		handlers: ROOT_HANDLER,
-		params: {},
-	});
-	expect(router.getRoute(makeGet('/posts'))).toMatchObject({
-		method: 'GET',
-		handlers: POSTS_HANDLER,
-		params: {},
-	});
-	expect(router.getRoute(makeGet('/posts/123'))).toMatchObject({
-		method: 'GET',
-		handlers: POST_HANDLER,
-		params: { id: '123' },
-	});
-	expect(router.getRoute(makePost('/posts/123'))).toMatchObject({
-		method: 'POST',
-		handlers: POST_UPDATE_HANDLER,
-		params: { id: '123' },
-	});
+	expect(router.getRoute(makeGet('/'))).toMatchObject(makeHandlerMatcher(GET, ROOT_HANDLER));
+	expect(router.getRoute(makeGet('/posts'))).toMatchObject(makeHandlerMatcher(GET, POSTS_HANDLER));
+	expect(router.getRoute(makeGet('/posts/123'))).toMatchObject(makeHandlerMatcher(GET, POST_HANDLER, ID_PARAM));
+	expect(router.getRoute(makePost('/posts/123'))).toMatchObject(makeHandlerMatcher(POST, POST_UPDATE_HANDLER, ID_PARAM));
+	expect(router.getRoute(makeGet('/optional/123'))).toMatchObject(makeHandlerMatcher(GET, OPTIONAL_PARAM_HANDLER, ID_PARAM));
+	expect(router.getRoute(makeGet('/optional'))).toMatchObject(makeHandlerMatcher(GET, OPTIONAL_PARAM_HANDLER));
 });

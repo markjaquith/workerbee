8434dacc26529783a0e8e253f663b6bbfb098938
Mark Jaquith <mark@jaquith.me>
2021-03-04 17:43:42 -0500

    Add tests for redirect(), setHost(), and setUrl()
    fixes #24

---
+++ src/handlers/redirect.test.ts
@@ -0,0 +1,20 @@
+import redirect from './redirect';
+
+const LOCATION = 'https://example.com/path/?foo=bar';
+const REQUEST = {
+	request: new Request(LOCATION),
+};
+
+test('redirect(301)', async () => {
+	const result = await redirect(301)(REQUEST);
+	expect(result).toBeInstanceOf(Response);
+	expect(result.status).toBe(301);
+	expect(result.headers.get('location')).toBe(LOCATION);
+});
+
+test('redirect(302)', async () => {
+	const result = await redirect(302)(REQUEST);
+	expect(result).toBeInstanceOf(Response);
+	expect(result.status).toBe(302);
+	expect(result.headers.get('location')).toBe(LOCATION);
+});
---
+++ src/handlers/setHost.test.ts
@@ -0,0 +1,13 @@
+import setHost from './setHost';
+
+const LOCATION = 'https://example.com/path/?foo=bar';
+const NEW_LOCATION = 'https://markjaquith.com/path/?foo=bar';
+const NEW_HOST = 'markjaquith.com';
+const REQUEST = {
+	request: new Request(LOCATION),
+};
+
+test('setHost()', async () => {
+	const result = await setHost(NEW_HOST)(REQUEST);
+	expect(new URL(result.url).toString()).toBe(NEW_LOCATION);
+});

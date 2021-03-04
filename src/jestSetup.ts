fc2471068f28f8895040f6eee9f200bd1cbd959d
Mark Jaquith <mark@jaquith.me>
2021-03-04 10:31:54 -0500

    Polyfill Response.redirect()

--- src/jestSetup.ts
+++ src/jestSetup.ts
@@ -1,3 +1,11 @@
 import fetchMock from 'jest-fetch-mock';
 
 fetchMock.enableMocks();
+
+Response.redirect = (url: string, status: number = 302) =>
+	new Response(null, {
+		headers: new Headers({
+			Location: url,
+			Status: `${status}`,
+		}),
+	});

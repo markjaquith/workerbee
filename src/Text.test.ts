7d33735afe4d30687ec4f71b0ba2e565f60400a3
Mark Jaquith <mark@jaquith.me>
2021-03-12 01:51:43 -0500

    Add toString() test for Text

--- src/Text.test.ts
+++ src/Text.test.ts
@@ -41,3 +41,9 @@ test('Text can be created using not.not.i()', () => {
 	expect(text.insensitive).toBe(true);
 	expect(text.negated).toBe(false);
 });
+
+test('Text can be seamlessly cast to a string', () => {
+	const text = Text.from('FOO');
+	expect(text.value).toBe('FOO');
+	expect(`${text}BAR`).toBe('FOOBAR');
+});

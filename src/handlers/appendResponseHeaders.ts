3e5606bb3f3630aa27b98c2536ca28f3d51273e3
Mark Jaquith <mark@jaquith.me>
2021-03-12 01:24:11 -0500

    Allow setResponseHeaders and appendResponseHeaders to accept a HeaderMap
    fixes #26

--- src/handlers/appendResponseHeaders.ts
+++ src/handlers/appendResponseHeaders.ts
@@ -4,6 +4,10 @@ export default function appendResponseHeaders(headers: Headers = []) {
 	return async function ({ response }) {
 		let newResponse;
 
+		if (!Array.isArray(headers)) {
+			headers = Object.entries(headers);
+		}
+
 		for (const [key, value] of headers) {
 			if (response.headers.get(key) !== value) {
 				newResponse = newResponse || new Response(response.body, response);
--- src/handlers/setResponseHeaders.ts
+++ src/handlers/setResponseHeaders.ts
@@ -1,10 +1,17 @@
 export type Header = [key: string, value: string];
-export type Headers = Header[];
+export interface HeaderMap {
+	[header: string]: string;
+}
+export type Headers = Header[] | HeaderMap;
 
 export default function setResponseHeaders(headers: Headers = []) {
 	return async function ({ response }) {
 		const newResponse = new Response(response.body, response);
 		let changed = false;
+
+		if (!Array.isArray(headers)) {
+			headers = Object.entries(headers);
+		}
 
 		for (const [key, value] of headers) {
 			if (newResponse.headers.get(key) !== value) {

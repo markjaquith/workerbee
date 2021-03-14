1ab1f36467a17ca0d32077fb85454ac5cec92cd9
Mark Jaquith <mark@jaquith.me>
2021-03-14 19:12:22 -0400

    Also test removing headers that are not present

--- src/handlers/removeResponseHeaders.test.ts
+++ src/handlers/removeResponseHeaders.test.ts
@@ -2,17 +2,26 @@ import removeResponseHeaders from './removeResponseHeaders';
 
 test('removeResponseHeaders()', async () => {
 	const removeFooAndBarHeaders = removeResponseHeaders(['foo', 'bar']);
-	const response = new Response('Response', {
+	const responseWithFooAndBar = new Response('Response', {
 		headers: new Headers({
 			foo: 'foo',
 			bar: 'bar',
 		}),
 	});
+	const responseWithoutFooAndBar = new Response('Response');
 
-	expect(response.headers.has('foo')).toBe(true);
-	expect(response.headers.has('bar')).toBe(true);
+	expect(responseWithFooAndBar.headers.has('foo')).toBe(true);
+	expect(responseWithFooAndBar.headers.has('bar')).toBe(true);
 
-	const result = await removeFooAndBarHeaders({ response });
+	const result = await removeFooAndBarHeaders({
+		response: responseWithFooAndBar,
+	});
+	expect(result.headers.has('foo')).toBe(false);
+	expect(result.headers.has('bar')).toBe(false);
+
+	const result2 = await removeFooAndBarHeaders({
+		response: responseWithoutFooAndBar,
+	});
 	expect(result.headers.has('foo')).toBe(false);
 	expect(result.headers.has('bar')).toBe(false);
 });

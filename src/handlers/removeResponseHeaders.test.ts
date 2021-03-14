8978813275ab6fe82e18bd65219297fc1fe567a9
Mark Jaquith <mark@jaquith.me>
2021-03-14 19:25:33 -0400

    More complete tests for removeResponseHeaders

--- src/handlers/removeResponseHeaders.test.ts
+++ src/handlers/removeResponseHeaders.test.ts
@@ -1,27 +1,53 @@
 import removeResponseHeaders from './removeResponseHeaders';
 
-test('removeResponseHeaders()', async () => {
-	const removeFooAndBarHeaders = removeResponseHeaders(['foo', 'bar']);
-	const responseWithFooAndBar = new Response('Response', {
+const removeFooAndBarHeaders = removeResponseHeaders(['foo', 'bar']);
+
+test('removeResponseHeaders() for existing headers', async () => {
+	const response = new Response('Response', {
 		headers: new Headers({
 			foo: 'foo',
 			bar: 'bar',
+			baz: 'baz',
 		}),
 	});
-	const responseWithoutFooAndBar = new Response('Response');
 
-	expect(responseWithFooAndBar.headers.has('foo')).toBe(true);
-	expect(responseWithFooAndBar.headers.has('bar')).toBe(true);
+	expect(response.headers.has('foo')).toBe(true);
+	expect(response.headers.has('bar')).toBe(true);
+	expect(response.headers.has('baz')).toBe(true);
 
-	const result = await removeFooAndBarHeaders({
-		response: responseWithFooAndBar,
-	});
+	const result = await removeFooAndBarHeaders({ response });
 	expect(result.headers.has('foo')).toBe(false);
 	expect(result.headers.has('bar')).toBe(false);
+	expect(result.headers.has('baz')).toBe(true);
+});
 
-	const result2 = await removeFooAndBarHeaders({
-		response: responseWithoutFooAndBar,
+test('removeResponseHeaders() when headers are not present', async () => {
+	const response = new Response('Response', {
+		headers: new Headers({
+			baz: 'baz',
+		}),
 	});
-	expect(result.headers.has('foo')).toBe(false);
-	expect(result.headers.has('bar')).toBe(false);
+
+	expect(response.headers.has('foo')).toBe(false);
+	expect(response.headers.has('bar')).toBe(false);
+	expect(response.headers.has('baz')).toBe(true);
+
+	const result = await removeFooAndBarHeaders({ response });
+	expect(result).toBeUndefined(); // Because there was nothing to change.
+});
+
+test('removeResponseHeaders() called without headers', async () => {
+	const removeNothing = removeResponseHeaders([]);
+	const response = new Response('Response', {
+		headers: new Headers({
+			baz: 'baz',
+		}),
+	});
+
+	expect(response.headers.has('foo')).toBe(false);
+	expect(response.headers.has('bar')).toBe(false);
+	expect(response.headers.has('baz')).toBe(true);
+
+	const result = await removeFooAndBarHeaders({ response });
+	expect(result).toBeUndefined(); // Because there was nothing to change.
 });

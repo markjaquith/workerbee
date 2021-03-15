bb9169b0c1c7f2f5d522c4cfe3a0304941940b30
Mark Jaquith <mark@jaquith.me>
2021-03-14 22:58:29 -0400

    Test default response for forbidden()

--- src/handlers/forbidden.test.ts
+++ src/handlers/forbidden.test.ts
@@ -1,8 +1,17 @@
 import forbidden from './forbidden';
 
-test('forbidden', async () => {
-	const response = await forbidden('Go away')();
-	expect(response).toBeInstanceOf(Response);
-	expect(response.status).toBe(403);
-	expect(await response.text()).toBe('Go away');
+describe('forbidden()', () => {
+	test('returns 403 and default message', async () => {
+		const response = await forbidden()();
+		expect(response).toBeInstanceOf(Response);
+		expect(response.status).toBe(403);
+		expect(await response.text()).toBe('Sorry, this page is not available.');
+	});
+
+	test('returns 403 and custom message', async () => {
+		const response = await forbidden('Go away')();
+		expect(response).toBeInstanceOf(Response);
+		expect(response.status).toBe(403);
+		expect(await response.text()).toBe('Go away');
+	});
 });

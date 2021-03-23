57d6fa9a663c74b91e579600b9957d4a755317b8
Mark Jaquith <mark@jaquith.me>
2021-03-22 21:17:14 -0400

    Simplify param tests

--- src/conditions/param.test.ts
+++ src/conditions/param.test.ts
@@ -1,15 +1,10 @@
-import { partialRight } from '../utils';
 import { endsWith, startsWith } from '../logic';
 import param from './param';
 
-const MESSAGE = {
-	url: new URL('https://x.co/?foo=bar-123&foo2&foo3=bar3-123'),
-};
-
-const applyParam = partialRight(param, [MESSAGE]);
+const REQUEST = new Request('https://x.co/?foo=bar-123&foo2&foo3=bar3-123');
 
 test('param', () => {
-	expect(applyParam('foo', startsWith('bar'))).toBe(true);
-	expect(applyParam('foo', endsWith('123'))).toBe(true);
-	expect(applyParam('baz', startsWith('foo'))).toBe(false);
+	expect(param('foo', startsWith('bar'), REQUEST)).toBe(true);
+	expect(param('foo', endsWith('123'), REQUEST)).toBe(true);
+	expect(param('baz', startsWith('foo'), REQUEST)).toBe(false);
 });
